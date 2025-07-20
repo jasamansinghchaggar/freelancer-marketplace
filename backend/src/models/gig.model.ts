35d2eb043cc8b4fd5734f8f7a04f6541c1ce83bd
jasamansinghchaggar <jsc.21905@gmail.com>
2025-07-20 17:39:12 +0530

    fix: backend api clean up

--- backend/src/models/gig.model.ts
+++ backend/src/models/gig.model.ts
@@ -4,7 +4,7 @@ export interface IGig extends Document {
     title: string;
     desc: string;
     price: number;
-    category: mongoose.Schema.Types.ObjectId; // No change needed here
+    category: mongoose.Schema.Types.ObjectId;
     fileId: string; 
     imageURL: string;
     userId: mongoose.Schema.Types.ObjectId;
@@ -14,7 +14,7 @@ const GigSchema: Schema = new Schema({
     title: { type: String, required: true },
     desc: { type: String, required: true },
     price: { type: Number, required: true },
-    category: { type: Schema.Types.ObjectId, ref: 'Category', required: true }, // No change needed here
+    category: { type: Schema.Types.ObjectId, ref: 'Category', required: true }, 
     fileId: { type: String, required: true }, 
     imageURL: { type: String, required: true },
     userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
